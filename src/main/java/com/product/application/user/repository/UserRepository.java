8e5e9803d76b7ce9ec59681d392f98defc8d1d39
JongBeom Park <jb753858@gmail.com>
2023-01-05 21:45:44 +0900

    feature : 회원가입시 닉네임 중복성 검사.

--- src/main/java/com/product/application/user/repository/UserRepository.java
+++ src/main/java/com/product/application/user/repository/UserRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByUseremail(String useremail);
+
+    Optional<Users> findByNickname(String nickname);
 }
