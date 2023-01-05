bf234aebe3a0228ca0a9ceef765a46c4d42cae72
JongBeom Park <jb753858@gmail.com>
2023-01-05 21:02:13 +0900

    feature : 로그인 API 생성 완료 + JWT 구현 완료.

---
+++ src/main/java/com/product/application/user/jwt/UserRoleEnum.java
@@ -0,0 +1,6 @@
+package com.product.application.user.jwt;
+
+public enum UserRoleEnum {
+    USER,  // 사용자 권한
+    ADMIN  // 관리자 권한
+}
--- src/main/java/com/product/application/user/service/UserService.java
+++ src/main/java/com/product/application/user/service/UserService.java
@@ -1,22 +1,26 @@
 package com.product.application.user.service;
 
 import com.product.application.common.exception.CustomException;
+import com.product.application.user.dto.LoginRequestDto;
 import com.product.application.user.dto.SignupRequestDto;
 import com.product.application.user.entity.Users;
+import com.product.application.user.jwt.JwtUtil;
 import com.product.application.user.mapper.UserMapper;
 import com.product.application.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
+import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
-import static com.product.application.common.exception.ErrorCode.DUPLICATE_USEREMAIL;
+import static com.product.application.common.exception.ErrorCode.*;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
-
+    private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
@@ -30,4 +34,18 @@ public class UserService {
         userRepository.save(users);
 
     }
+    @Transactional
+    public void login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        //사용자 확인
+        Users users = userRepository.findByUseremail(loginRequestDto.getUseremail()).orElseThrow(
+                () -> new CustomException(USEREMAIL_NOT_FOUND)
+        );
+        //비밀번호 확인
+        if(!users.getPassword().equals(loginRequestDto.getPassword())){
+            throw new CustomException(INCORRECT_PASSWORD);
+        }
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(users.getUseremail()));
+    }
+
 }
