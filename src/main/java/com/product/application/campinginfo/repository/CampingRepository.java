03e5c20c9ee49de5f7dcc33c52cc4245227195af
Inventor <developer7098@gmail.com>
2023-01-06 20:10:32 +0900

    feature : 리뷰 수정기능 작성(1차완료) - 캠핑 포스트 생성 기능 추가 후 테스트하고 push할 예정

--- src/main/java/com/product/application/campinginfo/repository/CampingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.product.application.campinginfo.repository;
-
-import com.product.application.campinginfo.entity.Camping;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampingRepository extends JpaRepository<Camping, Long> {
-}
