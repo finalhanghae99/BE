58d515ae717fadd2acfd8ffdfcd052cb3caacda5
jinhyoje <jjjhhh810@naver.com>
2023-01-07 02:00:39 +0900

    feature : 양도 글 삭제

--- src/main/java/com/product/application/reservation/service/ReservationService.java
+++ src/main/java/com/product/application/reservation/service/ReservationService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +23,7 @@ public class ReservationService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
+    @Transactional
     public void create(ReservationRequestDto reservationRequestDto, HttpServletRequest request) {
         String token = jwtUtil.resolveToken(request);
         Claims claims;
@@ -39,10 +41,37 @@ public class ReservationService {
                     () -> new CustomException(ErrorCode.USER_NOT_FOUND)
             );
 
-            Camping camping = new Camping();
-            Reservation reservation = reservationRepository.save(new Reservation(reservationRequestDto, users));
+            reservationRepository.save(new Reservation(reservationRequestDto, users));
 
-        }else{
+        } else {
+            throw new CustomException(ErrorCode.TOKEN_ERROR);
+        }
+    }
+
+    @Transactional
+    public void delete(Long reservationId, HttpServletRequest request) {
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+
+        if (token != null) {
+            // Token 검증
+            if (jwtUtil.validateToken(token)) {
+                // 토큰에서 사용자 정보 가져오기
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("Token Error");
+            }
+            Users users = userRepository.findByUseremail(claims.getSubject()).orElseThrow(
+                    () -> new CustomException(ErrorCode.USER_NOT_FOUND)
+            );
+
+            Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(
+                    () -> new CustomException(ErrorCode.CONTENT_NOT_FOUND)
+            );
+
+            reservationRepository.deleteById(reservationId);
+
+        } else {
             throw new CustomException(ErrorCode.TOKEN_ERROR);
         }
     }
