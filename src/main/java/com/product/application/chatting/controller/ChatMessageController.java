88705ad0c2ca179c6e1113d3ce7a6fbf1381ca99
jinhyoje <jjjhhh810@naver.com>
2023-01-30 18:21:01 +0900

    refactoring : CrossOrigin 붙임

--- src/main/java/com/product/application/chatting/controller/ChatMessageController.java
+++ src/main/java/com/product/application/chatting/controller/ChatMessageController.java
@@ -3,13 +3,11 @@ package com.product.application.chatting.controller;
 import com.product.application.chatting.dto.RequestMessageDto;
 import com.product.application.chatting.service.ChatService;
 import com.product.application.common.ResponseMessage;
+import com.product.application.user.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -17,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatMessageController {
     private final SimpMessageSendingOperations sendingOperations;
     private final ChatService chatService;
+    @CrossOrigin(origins = {"http://campingzipbeta.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000"}, exposedHeaders = JwtUtil.AUTHORIZATION_HEADER)
     @MessageMapping("/chat/message/{reservationId}/{roomId}/{nickname}")
     public ResponseMessage<?> saveMessage(@RequestBody RequestMessageDto requestMessageDto,
                                           @PathVariable Long reservationId,
--- src/main/java/com/product/application/chatting/controller/ChatRoomController.java
+++ src/main/java/com/product/application/chatting/controller/ChatRoomController.java
@@ -3,6 +3,7 @@ package com.product.application.chatting.controller;
 import com.product.application.chatting.dto.ResponseChatListDto;
 import com.product.application.chatting.service.ChatService;
 import com.product.application.common.ResponseMessage;
+import com.product.application.user.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 public class ChatRoomController {
 
     private final ChatService chatService;
+    @CrossOrigin(origins = {"http://campingzipbeta.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000"}, exposedHeaders = JwtUtil.AUTHORIZATION_HEADER)
     @PostMapping("/{reservationId}")
     public ResponseMessage<?> createRoom(HttpServletRequest request, @PathVariable Long reservationId) {
         chatService.createRoom(request, reservationId);
@@ -22,25 +24,26 @@ public class ChatRoomController {
     }
 
     //채팅방에 있는 양도글 정보
+    @CrossOrigin(origins = {"http://campingzipbeta.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000"}, exposedHeaders = JwtUtil.AUTHORIZATION_HEADER)
     @GetMapping("/room/{reservationId}/{roomId}")
     @ResponseBody
     public ResponseMessage reservationInfo(@PathVariable String roomId, @PathVariable Long reservationId){
         ResponseMessage responseMessage = chatService.reservationInfo(roomId, reservationId);
         return  responseMessage;
     }
-
+    @CrossOrigin(origins = {"http://campingzipbeta.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000"}, exposedHeaders = JwtUtil.AUTHORIZATION_HEADER)
     @GetMapping("/{roomId}")
     public ResponseMessage getnickname(@PathVariable String roomId, HttpServletRequest request){
         ResponseMessage responseMessage = chatService.getnickname(roomId, request);
         return responseMessage;
     }
-
+    @CrossOrigin(origins = {"http://campingzipbeta.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000"}, exposedHeaders = JwtUtil.AUTHORIZATION_HEADER)
     @GetMapping("/mypage/chatting")
     public ResponseMessage<?> userChattingInfo(HttpServletRequest request){
         ResponseChatListDto responseChatListDto = chatService.userChattingInfo(request);
         return  new ResponseMessage<>("Success", 200, responseChatListDto);
     }
-
+    @CrossOrigin(origins = {"http://campingzipbeta.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000"}, exposedHeaders = JwtUtil.AUTHORIZATION_HEADER)
     // 특정 채팅방 내용 조회
     @GetMapping("/room/{roomId}")
     @ResponseBody
