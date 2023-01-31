b6220e7a253c792c55b3ebd306010c1719072683
jinhyoje <jjjhhh810@naver.com>
2023-01-31 21:10:10 +0900

    refactoring : 채팅방 생성 유무

--- src/main/java/com/product/application/chatting/repository/ChatRoomRepository.java
+++ src/main/java/com/product/application/chatting/repository/ChatRoomRepository.java
@@ -3,8 +3,12 @@ package com.product.application.chatting.repository;
 import com.product.application.chatting.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
     ChatRoom findByRoomId(String roomId);
+
+    List<ChatRoom> findAllByBuyerAndSellerAndReservationId(String buyer, String seller, Long reservationId);
 
 }
